79a60019c69cbb11b3f07978dfe8dc1eee3fcbfe
Marzi Ghasedian <marzighased@gmail.com>
2025-05-10 22:13:23 +0200

    Array of Quotes added

---
+++ script.js
@@ -0,0 +1,30 @@
+const quotes = [
+    {
+        text: "It doesn't matter how slowly you go as long as you don't stop.",
+        author: "Confucius"
+    },
+    {
+        text: "Life is what happens when you're busy making other plans.",
+        author: "John Lennon"
+    },
+    {
+        text: "Success is not the key to happiness. Happiness is the key to success.",
+        author: "Albert Schweitzer"
+    },
+    {
+        text: "The only way to do great work is to love what you do.",
+        author: "Steve Jobs"
+    },
+    {
+        text: "The biggest risk is not taking any risk.",
+        author: "Mark Zuckerberg"
+    },
+    {
+        text: "The best revenge is massive success.",
+        author: "Frank Sinatra"
+    },
+    {
+        text: "If what you did yesterday still looks big to you, you haven't done much today.",
+        author: "Bill Gates"
+    }
+];
